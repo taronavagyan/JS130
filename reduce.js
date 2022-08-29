984c1b1aaa3a0bac15ae9d4a514cc47ee69c8a31
Taron Avagyan <contact@taronavagyan.com>
2022-08-29 15:51:45 -0700

    filtering with reduce

--- reduce.js
+++ reduce.js
@@ -1,30 +1,47 @@
-function reduce(array, callback, accum) {
-  let idx = accum ? 0 : 1;
-  accum = accum || array[0];
-  while (idx < array.length) {
-    accum = callback(accum, array[idx]);
-    idx += 1;
-  }
-  return accum;
-}
+// function reduce(array, callback, accum) {
+//   let idx = accum ? 0 : 1;
+//   accum = accum || array[0];
+//   while (idx < array.length) {
+//     accum = callback(accum, array[idx]);
+//     idx += 1;
+//   }
+//   return accum;
+// }
 
-let numbers = [1, 2, 3, 4, 5];
-console.log(reduce(numbers, (accum, number) => accum + number)); // => 15
-console.log(reduce(numbers, (prod, number) => prod * number)); // => 120
-console.log(reduce(numbers, (prod, number) => prod * number, 3)); // => 360
-console.log(reduce([], (accum, number) => accum + number, 10)); // => 10
-console.log(reduce([], (accum, number) => accum + number));
-// => undefined
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(reduce(numbers, (accum, number) => accum + number)); // => 15
+// console.log(reduce(numbers, (prod, number) => prod * number)); // => 120
+// console.log(reduce(numbers, (prod, number) => prod * number, 3)); // => 360
+// console.log(reduce([], (accum, number) => accum + number, 10)); // => 10
+// console.log(reduce([], (accum, number) => accum + number));
+// // => undefined
 
-let stooges = ["Mo", "Larry", "Curly"];
-console.log(
-  reduce(
-    stooges,
-    (reversedStooges, stooge) => {
-      reversedStooges.unshift(stooge);
-      return reversedStooges;
-    },
-    []
-  )
-);
-// => ["Curly", "Larry", "Mo"]
+// let stooges = ["Mo", "Larry", "Curly"];
+// console.log(
+//   reduce(
+//     stooges,
+//     (reversedStooges, stooge) => {
+//       reversedStooges.unshift(stooge);
+//       return reversedStooges;
+//     },
+//     []
+//   )
+// );
+// // => ["Curly", "Larry", "Mo"]
+
+// Filter with reduce
+
+// function filter(array, conditional) {
+//   return array.reduce(function (acc, elem) {
+//     return conditional(elem) ? [...acc, elem] : [...acc];
+//   }, []);
+// }
+
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(filter(numbers, (number) => number > 3)); // => [ 4, 5 ]
+// console.log(filter(numbers, (number) => number < 0)); // => []
+// console.log(filter(numbers, () => true)); // => [ 1, 2, 3, 4, 5 ]
+
+// let values = [1, "abc", null, true, undefined, "xyz"];
+// console.log(filter(values, (value) => typeof value === "string"));
+// // => [ 'abc', 'xyz' ]
