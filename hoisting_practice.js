5fb7c1f42cd4f6d2f5a65252aa211235e4767761
Taron Avagyan <contact@taronavagyan.com>
2022-10-09 15:15:02 -0700

    3

--- hoisting_practice.js
+++ hoisting_practice.js
@@ -1,9 +1,33 @@
-var foo = function () {
-  console.log("Bye");
-};
+// 1.
 
-function foo() {
-  console.log("Hello");
+// var foo = function () {
+//   console.log("Bye");
+// };
+
+// function foo() {
+//   console.log("Hello");
+// }
+
+// foo(); // "Bye"
+
+// 2.
+
+// for (var index = 0; index < 2; index += 1) {
+//   console.log(foo);
+//   if (index === 0) {
+//     var foo = "Hello";
+//   } else {
+//     foo = "Bye";
+//   }
+// }
+
+// console.log(foo);
+// console.log(index);
+
+// 3.
+
+bar();
+
+function bar() {
+  console.log("foo!");
 }
-
-foo(); // "Bye"
