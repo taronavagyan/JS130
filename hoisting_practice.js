d5f26435a39f537db4f542b28f21fd31b1315a8e
Taron Avagyan <contact@taronavagyan.com>
2022-10-09 15:27:55 -0700

    #6

--- hoisting_practice.js
+++ hoisting_practice.js
@@ -71,3 +71,27 @@
 
 // foo(true);
 // foo(false);
+
+// 6.
+
+// function Pet(name, image) {
+//   this.name = name;
+//   this.image = image;
+// }
+
+// let Image;
+// var catImage;
+// var pudding;
+
+// Pet.prototype.walk = function () {
+//   console.log(`${this.name} is walking.`);
+// };
+
+// Image = class {
+//   constructor(file) {
+//     this.file = file;
+//   }
+// };
+
+// catImage = new Image("cat.png");
+// pudding = new Pet("Pudding", catImage);
