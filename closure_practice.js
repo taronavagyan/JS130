36f7132705585ce3161450038442a2068c85c042
Taron Avagyan <contact@taronavagyan.com>
2022-10-10 12:54:01 -0700

    #2

--- closure_practice.js
+++ closure_practice.js
@@ -1,18 +1,36 @@
 // 1.
 
-let counter = 0;
+// let counter = 0;
 
-function makeCounter() {
-  return function () {
-    counter += 1;
-    return counter;
-  };
-}
+// function makeCounter() {
+//   return function () {
+//     counter += 1;
+//     return counter;
+//   };
+// }
 
-let incrementCounter = makeCounter();
-console.log(incrementCounter());
-console.log(incrementCounter());
+// let incrementCounter = makeCounter();
+// console.log(incrementCounter()); // 1
+// console.log(incrementCounter()); // 2
 
-incrementCounter = makeCounter();
-console.log(incrementCounter());
-console.log(incrementCounter());
+// incrementCounter = makeCounter();
+// console.log(incrementCounter()); // 3
+// console.log(incrementCounter()); // 4
+
+// 2.
+
+// function makeCounter() {
+//   return function () {
+//     let counter = 0;
+//     counter += 1;
+//     return counter;
+//   };
+// }
+
+// let incrementCounter = makeCounter();
+// console.log(incrementCounter()); // 1
+// console.log(incrementCounter()); // 1
+
+// incrementCounter = makeCounter();
+// console.log(incrementCounter()); // 1
+// console.log(incrementCounter()); // 1
