57fd4e45de1b1e1bf4743dfe41a1e1f7195a7fb1
Taron Avagyan <contact@taronavagyan.com>
2022-10-10 10:59:33 -0700

    experiments

--- closure_notes.js
+++ closure_notes.js
@@ -1,16 +1,36 @@
-function download(locationOfFile, errorHandler) {
-  // try to download the file
-  let gotError = true;
-  let reasonCode = "Cannot reach server";
-  if (gotError) {
-    errorHandler(reasonCode);
-  }
-}
+// function download(locationOfFile, errorHandler) {
+//   // try to download the file
+//   let gotError = true;
+//   let reasonCode = "Cannot reach server";
+//   if (gotError) {
+//     errorHandler(reasonCode);
+//   }
+// }
 
-function errorDetected(url, reason) {
-  // handle the error
-  console.log(url, "!!!", reason);
-}
+// function errorDetected(url, reason) {
+//   // handle the error
+//   console.log(url, "!!!", reason);
+// }
 
-let url = "https://example.com/foo.txt";
-download(url, errorDetected.bind(null, url));
+// let url = "https://example.com/foo.txt";
+// download(url, errorDetected.bind(null, url));
+
+//
+
+// function makeLogger1(identifier) {
+//   return function (msg) {
+//     console.log(identifier + " " + msg);
+//   };
+// }
+
+// function makeLogger2(identifier) {
+//   return function (msg) {
+//     console.log(identifier, msg);
+//   };
+// }
+
+// let logger1 = makeLogger1("Red");
+// let logger2 = makeLogger2("Green");
+
+// logger1("Alert");
+// logger2("Alert");
