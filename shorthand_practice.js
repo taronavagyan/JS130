c8d8e8b54183ba6fbbe41f2eba9ff9aa0c0224b8
Taron Avagyan <contact@taronavagyan.com>
2022-10-11 16:20:27 -0700

    #4-6

--- shorthand_practice.js
+++ shorthand_practice.js
@@ -42,3 +42,34 @@
 // let baz = obj.baz;
 // let qux = obj.qux;
 // let bar = obj.bar;
+
+// 4.
+
+// function foo(arr) {
+//   return [arr[2], 5, arr[0]];
+// }
+
+// let array = [1, 2, 3];
+// let result = foo(array);
+// let bar = result[0];
+// let qux = result[1];
+// let baz = result[2];
+
+// 5.
+
+// function product(num1, num2, num3) {
+//   return num1 * num2 * num3;
+// }
+
+// let array = [2, 3, 5];
+// let result = product(arr[0], arr[1], arr[2]);
+
+// 6.
+
+// function product() {
+//   return [].reduce.call(arguments, (a, b) => a * b, arguments[0]);
+// }
+
+// let result = product(2, 3, 4, 5);
+
+// 7.
