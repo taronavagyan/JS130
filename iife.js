cab22a8352744b578e9a88d97995cdf7bd7343c0
Taron Avagyan <contact@taronavagyan.com>
2022-10-11 15:49:51 -0700

    #3

--- iife.js
+++ iife.js
@@ -11,3 +11,20 @@
 // (function () {
 //   console.log("Sometimes, syntax isn't intuitive!");
 // })();
+
+// 3.
+
+// var sum = 0;
+// sum += 10;
+// sum += 31;
+
+// let numbers = [1, 7, -3, 3];
+
+// sum += (function sum(arr) {
+//   return arr.reduce((sum, number) => {
+//     sum += number;
+//     return sum;
+//   }, 0);
+// })(numbers);
+
+// console.log(sum);
