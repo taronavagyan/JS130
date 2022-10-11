5ccf67162441503b1a408a1a2f1a0c5573c59818
Taron Avagyan <contact@taronavagyan.com>
2022-10-11 15:45:06 -0700

    #1-2

---
+++ iife.js
@@ -0,0 +1,13 @@
+// 1.
+
+// function() {
+//   console.log("Sometimes, syntax isn't intuitive!");
+// }();
+
+// The snippet won't execute because the function declaration must be converted to a function expression before it's invoked as an IIFE.
+
+// 2.
+
+// (function () {
+//   console.log("Sometimes, syntax isn't intuitive!");
+// })();
