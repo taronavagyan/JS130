e87b62b6ff4a89b0015aecaaf6a57ddda5c71326
Taron Avagyan <contact@taronavagyan.com>
2022-10-12 21:49:51 -0700

    misc #1-2

--- practice_problems.js
+++ practice_problems.js
@@ -71,3 +71,26 @@
 // };
 
 // baz.qux(); // logs 'changed'
+
+//// Miscellaneous ////
+
+// 1.
+
+// function sum(...values) {
+//   return values.reduce(function (a, b) {
+//     return a + b;
+//   });
+// }
+
+// sum(1, 4, 5, 6); // 16
+
+// 2.
+
+// function formatName(firstName, middleName, lastName) {
+//   return `${lastName}, ${firstName} ${middleName[0]}.`;
+// }
+
+// fullName = ["James", "Tiberius", "Kirk"];
+
+// console.log(formatName(...fullName));
+// // logs: Kirk, James T.
