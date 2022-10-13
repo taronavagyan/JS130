bfdb41b1c17ff26e58e3db4c113f8fffcc8b671a
Taron Avagyan <contact@taronavagyan.com>
2022-10-13 13:34:05 -0700

    #5

--- practice_problems.js
+++ practice_problems.js
@@ -72,6 +72,95 @@
 
 // baz.qux(); // logs 'changed'
 
+// 5.
+
+// const Account = {
+
+//   init(email, password, firstName, lastName) {
+
+//     return (function () {
+//       let displayName = (function anonymize() {
+//         const characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+//         let randomStr = "";
+      
+//         while (randomStr.length < 16) {
+//           let randomChar = characters[Math.floor(Math.random() * characters.length)];
+//           randomStr += randomChar;
+//         }
+      
+//         return randomStr;
+//       })();
+
+//       return {
+        
+//         displayName,
+
+//         reanonymize(currentPassword) {
+//           if (password !== currentPassword) return "Invalid Password!";
+//           const characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+//           let randomStr = "";
+        
+//           while (randomStr.length < 16) {
+//             let randomChar = characters[Math.floor(Math.random() * characters.length)];
+//             randomStr += randomChar;
+//           }
+        
+//           this.displayName = randomStr;
+//           return true;
+//         },
+
+//         resetPassword(currentPassword, newPassword) {
+//           if (currentPassword !== password) return "Invalid Password";
+//           password = newPassword;
+//           return true;
+//         },
+
+//         firstName(currentPassword) {      
+//           if (password !== currentPassword) return "Invalid Password";
+//           return firstName;
+//         },
+
+//         lastName(currentPassword) {
+//           if (password !== currentPassword) return "Invalid Password";
+//           return lastName;
+//         },
+
+//         email(currentPassword) {
+//           if (password !== currentPassword) return "Invalid Password";
+//           return email;
+//         }, 
+        
+//       }
+//     })();
+//   },
+
+
+
+// };
+
+// let fooBar = Object.create(Account).init('foo@bar.com', '123456', 'foo', 'bar');
+// console.log(fooBar.firstName);                     // returns the firstName function
+// console.log(fooBar.email);                         // returns the email function
+// console.log(fooBar.firstName('123456'));           // logs 'foo'
+// console.log(fooBar.firstName('abc'));              // logs 'Invalid Password'
+// console.log(fooBar.displayName);                   // logs 16 character sequence
+// console.log(fooBar.resetPassword('123', 'abc'))    // logs 'Invalid Password'
+// console.log(fooBar.resetPassword('123456', 'abc')) // logs true
+
+// let displayName = fooBar.displayName;
+// fooBar.reanonymize('abc');                         // returns true
+// console.log(displayName === fooBar.displayName);   // logs false
+
+// let bazQux = Object.create(Account).init('baz@qux.com', '123456', 'baz', 'qux');
+// // Note that the foillowing two lines of code are correct as written.
+// // There are no mistakes. We are attempting to demonstrate that the
+// // code does not work as might be intended.
+// console.log(fooBar.firstName('abc'));           // logs 'baz' but should log foo.
+// console.log(fooBar.email('abc'));               // 'baz@qux.com' but should 'foo@bar.com'
+
+
+
+
 //// Miscellaneous ////
 
 // 1.
