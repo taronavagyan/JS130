9c96f80908a41b50262883de38a9862a5f4c2762
Taron Avagyan <contact@taronavagyan.com>
2022-10-12 19:10:34 -0700

    #2

--- practice_problems.js
+++ practice_problems.js
@@ -5,3 +5,16 @@
 //     func.call(obj, arguments);
 //   };
 // }
+
+// 2.
+
+// function myBind(func, ctx) {
+//   let partialArgs = [].slice.apply(arguments, [2]);
+
+//   return function () {
+//     let remainingArgs = [].slice.apply(arguments);
+//     let fullArgs = partialArgs.concat(remainingArgs);
+
+//     return func.apply(ctx, fullArgs);
+//   };
+// }
