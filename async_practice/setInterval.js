90dc78df3c1c312957e8b4ba6423dba88f095821
Taron Avagyan <contact@taronavagyan.com>
2022-10-17 16:08:42 -0700

    pp 1-2

--- async_practice/setInterval.js
+++ async_practice/setInterval.js
@@ -1,1 +1,28 @@
 // 1.
+
+// (function startCounting() {
+//   let count = 1;
+//   setInterval(() => {
+//     console.log(count);
+//     count += 1;
+//   }, 1000);
+// })();
+
+// 2.
+
+// function startCounting() {
+//   let count = 1;
+//   let counterID = setInterval(() => {
+//     console.log(count);
+//     count += 1;
+//   }, 1000);
+//   return counterID;
+// }
+
+// function stopCounting(counterID) {
+//   clearInterval(counterID);
+// }
+
+// let counterID = startCounting();
+
+// stopCounting(counterID);
