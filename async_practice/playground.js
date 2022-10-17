cfc0e912d302240d2bca71b4721124750bd37324
Taron Avagyan <contact@taronavagyan.com>
2022-10-17 11:55:12 -0700

    practice problems

---
+++ async_practice/playground.js
@@ -0,0 +1,32 @@
+// function logger(object) {
+//   // define a Function logger
+//   console.log(object); // that logs its parameter
+// }
+
+// let numbers = [3, 7, 25, 39]; // set numbers to [3, 7, 25, 39]
+// numbers.forEach(logger); // call forEach with logger as a callback
+// console.log(numbers.length); // log 4
+
+///
+
+// setTimeout(function () {
+//   console.log("!");
+// }, 3000);
+
+// setTimeout(function () {
+//   console.log("World");
+// }, 1000);
+
+// console.log("Hello");
+
+///
+
+// setTimeout(function () {
+//   console.log("!");
+// }, 0);
+
+// setTimeout(function () {
+//   console.log("World");
+// }, 0);
+
+// console.log("Hello");
---
+++ async_practice/practice_problems.js
@@ -0,0 +1,77 @@
+// 1.
+
+// (function delayLog() {
+//   for (var num = 1; num <= 10; num += 1) {
+//     setTimeout(() => console.log(num), num * 1000);
+//   }
+// })();
+
+// 2.
+
+// 11 is logged each time because var is function scoped
+
+// 3.
+
+// setTimeout(function() {   // 1
+//   console.log('Once');    // 5
+// }, 1000);
+
+// setTimeout(function() {   // 2
+//   console.log('upon');    // 7
+// }, 3000);
+
+// setTimeout(function() {   // 3
+//   console.log('a');       // 6
+// }, 2000);
+
+// setTimeout(function() {   // 4
+//   console.log('time');    // 8
+// }, 4000);
+
+// 4.
+
+// function g() {}
+// function f() {}
+// function z() {}
+// function d() {}
+// function n() {}
+// function q() {
+//   console.log("q");
+// }
+// function s() {
+//   console.log("s");
+// }
+// setTimeout(function () {
+//   setTimeout(function () {
+//     q();
+//   }, 15);
+
+//   d();
+
+//   setTimeout(function () {
+//     n();
+//   }, 5);
+
+//   z();
+// }, 10);
+
+// setTimeout(function () {
+//   s();
+// }, 20);
+
+// setTimeout(function () {
+//   f();
+// });
+
+// g();
+
+// g(), f(), d(), z(), n(), s(), q()
+
+// 5.
+
+// function afterNSeconds(callback, ms) {
+//   let secs = ms * 1000;
+//   setTimeout(callback, secs);
+// }
+
+// afterNSeconds(() => console.log("boom!"), 3);
