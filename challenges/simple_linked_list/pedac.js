edb77cc2b2d3075ba9146b2b01bfabf62b3ff1df
Taron Avagyan <contact@taronavagyan.com>
2022-10-27 11:00:56 -0700

    .

--- challenges/simple_linked_list/pedac.js
+++ challenges/simple_linked_list/pedac.js
@@ -1,3 +1,7 @@
 // P
 
 // Write a simple linked list implementation. The linked list is a fundamental data structure in computer science, often used in the implementation of other data structures.
+
+// The simplest kind of linked list is a singly linked list. Each element in the list contains data and a "next" field pointing to the next element in the list of elements. This variant of linked lists is often used to represent sequences or push-down stacks (also called a LIFO stack; Last In, First Out).
+
+// Let's create a singly linked list whose elements may contain a range of data such as the numbers 1-10. Provide functions to reverse the linked list and convert a linked list to and from an array.
