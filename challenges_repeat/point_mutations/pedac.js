4cee29579e57aa234ea996a7882857eb5851d702
Taron Avagyan <contact@taronavagyan.com>
2022-11-03 14:04:32 -0700

    .

---
+++ challenges_repeat/point_mutations/pedac.js
@@ -0,0 +1,21 @@
+// P
+
+// Write a program that can calculate the Hamming distance between two DNA strands.
+
+// Find differences in 2 strings
+
+// The Hamming distance is only defined for sequences of equal length. If you have two sequences of unequal length, you should compute the Hamming distance over the shorter length.
+
+// E
+
+// GAGCCTACTAACGGGAT
+// CATCGTAATGACGGCCT
+// ^ ^ ^  ^ ^    ^^
+
+// The Hamming distance between these two DNA strands is 7.
+
+// D
+
+// dna class
+// constructor takes string
+// hammingDistance instance method (takes string), return num
